f2d8107469c11c19c5224b24ab0b70aeefe3bc2f
BMCC-CSC350-SP21-D <80847402+BMCC-CSC350-SP21-D@users.noreply.github.com>
2021-05-23 20:51:29 -0400

    Update savedetails.php

--- savedetails.php
+++ savedetails.php
@@ -2,19 +2,33 @@
 $date = $_POST['day'];
 $time = $_POST['time'];
 $name = $_POST['name'];
+$email = $_POST['Email'];
 $conn = new mysqli("localhost","root","root","info");
-if ($conn->connect_error) {
+if ($conn->connect_error) 
+{
   die("Connection failed: " . $conn->connect_error);
 }
-$sql = "insert into appointment (date,time,Name)
-values ('$date','$time','$name')";
-$sql = "SELECT appointment
-FROM user
-JOIN appointment ON user.tenantName=appointment.Name";
-if (mysqli_query($conn, $sql)) {
-  echo "Appointment created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+else
+{
+	$query = "SELECT * FROM appointment WHERE date='$date' AND time ='$time'";
+	$results = mysqli_query($conn, $query);
+	if(mysqli_num_rows($results) != 1)
+	{
+		$sql = "insert into appointment (Name,date,time,email)
+		values ('$name','$date','$time','$email')";
+		if (mysqli_query($conn, $sql)) 
+		{
+			echo "Appointment created successfully";
+		}
+		else 
+		{
+			echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+	}
+	else
+	{
+		echo"Error: The day/time slot has already been reserved";
+		echo "<br>";
+	}
 }
-
 ?>
