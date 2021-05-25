3242e55a2dc98dd429744286805576ab504460d8
BMCC-CSC350-SP21-D <80847402+BMCC-CSC350-SP21-D@users.noreply.github.com>
2021-05-24 21:39:54 -0400

    Update welcome.php

--- Code/welcome.php
+++ Code/welcome.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
@@ -5,6 +6,17 @@
   box-sizing: border-box;
 }
 
+/*this is background picture */
+.background{
+  min-height: 100vh;
+  width: 100%;
+  background-image: linear-gradient(rgba(4,9,30,0.7),rgba(4,9,30,0.7)),url(background.png);
+  background-position: center;
+  background-size: cover;
+  position: relative; 
+}
+
+/*all the input styles */
 input[type=text], select, textarea {
   width: 100%;
   padding: 15px;
@@ -12,12 +24,13 @@ input[type=text], select, textarea {
   border-radius: 4px;
   resize: vertical;
 }
-
+/*label styling  */
 label {
   padding: 12px 12px 12px 0;
   display: inline-block;
+  color: #fff;
 }
-
+/*submit button styles  */
 input[type=submit] {
   background-color: #04AA6D;
   color: white;
@@ -28,35 +41,37 @@ input[type=submit] {
 
  
 }
-
+/*aubmit button with hover styling*/
 input[type=submit]:hover {
   background-color: #45a049;
 }
-
+/*the appointment form styles */
 .container {
   border-radius: 5px;
-  background-color: #f2f2f2;
+  background-color: transparent;
   padding: 20px;
 }
-
 .col-25 {
   float: left;
   width: 25%;
   margin-top: 6px;
 }
-
 .col-75 {
   float: left;
   width: 75%;
   margin-top: 6px;
 }
-
 /* Clear floats after the columns */
 .row:after {
   content: "";
   display: table;
   clear: both;
   
+}
+.background h2{
+  color: #fff;
+  padding: 10px;
+  text-align: center; 
 }
 
 /* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
@@ -69,31 +84,32 @@ input[type=submit]:hover {
 </style>
 </head>
 <body>
-
-<h2>Fill this out to make an appointment</h2>
-<form action="savedetails.php" method="post">
-<div class="container">
+<section class="background">
+<h2>Fill this out to make an appointment</h2> <!-- this the title of the appointment page -->
+<form action="savedetails.php" method="post"> <!-- links to database after the submit is pressed -->
+<div class="container"> <!-- all code from here is whats in the container for appointment  -->
     <div class="row">
       <div class="col-25">
-        <label for="name">Full Name</label>
+        <label for="name">Full Name</label> <!-- lable for the name  -->
       </div>
       <div class="col-75">
-        <input type="text" id="name" name="name" placeholder="Your full name..">
+        <input type="text" id="name" name="name" placeholder="Your full name.."required><!-- name text box -->
       </div>
     </div>
     <div class="row">
       <div class="col-25">
-        <label for="lname">Email</label>
+        <label for="lname">Email</label> <!-- lable for the email -->
       </div>
       <div class="col-75">
-        <input type="text" id="lname" name="Email" placeholder="Email">
+        <input type="text" id="Email" name="Email" placeholder="Email"required><!-- email text box  -->
       </div>
     <div class="row">
       <div class="col-25">
-        <label for="unit">unit</label>
+        <label for="unit">unit</label> <!-- label for the unit  -->
       </div>
       <div class="col-75">
-        <select id="unit" name="unit">
+        <select id="unit" name="unit"> <!-- list of all the the units -->
+          <option value="0">0</option>
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -119,42 +135,41 @@ input[type=submit]:hover {
           <option value="23">23</option>
           <option value="24">24</option>
           <option value="25">25</option>
-
           
         </select>
       </div>
     </div>
     <div class="row">
       <div class="col-25">
-  <p>washing time:</p>
+  <label for="washing time">washing time:</label> 
   <div>
-	<select id="day" name="day">
-		<option value="monday">Monday</option>
-		<option value="tuesday">Tuesday</option>
-		<option value="wednesday">Wednesday</option>
-		<option value="thursday">Thursday</option>
-		<option value="friday">Friday</option>
-		<option value="saturday">Saturday</option>
-		<option value="sunday">Sunday</option>
-	</select>
+  <select id="day" name="day"><!-- days of the week list -->
+    <option value="monday">Monday</option>
+    <option value="tuesday">Tuesday</option>
+    <option value="wednesday">Wednesday</option>
+    <option value="thursday">Thursday</option>
+    <option value="friday">Friday</option>
+    <option value="saturday">Saturday</option>
+    <option value="sunday">Sunday</option>
+  </select>
   </div>
   <div>
-	<select id="time" name="time">
-		<option value="12AM-3AM">12AM-3AM</option>
-		<option value="3AM-6AM">3AM-6AM</option>
-		<option value="6AM-9AM">6AM-9AM</option>
-		<option value="9AM-12PM">9AM-12PM</option>
-		<option value="12PM-3PM">12PM-3PM</option>
-		<option value="3PM-6PM">3PM-6PM</option>
-		<option value="6PM-9PM">6PM-9PM</option>
-		<option value="9PM-12AM">9PM-12AM</option>
-	</select>
+  <select id="time" name="time"> <!-- list of  time  -->
+    <option value="12AM-3AM">12AM-3AM</option>
+    <option value="3AM-6AM">3AM-6AM</option>
+    <option value="6AM-9AM">6AM-9AM</option>
+    <option value="9AM-12PM">9AM-12PM</option>
+    <option value="12PM-3PM">12PM-3PM</option>
+    <option value="3PM-6PM">3PM-6PM</option>
+    <option value="6PM-9PM">6PM-9PM</option>
+    <option value="9PM-12AM">9PM-12AM</option>
+  </select>
   </div>
     <div class="row">
-      <input type="submit" value="Submit">
+      <input type="submit" value="Submit"> <!-- submit button -->
     </div>
   </form>
 </div>
-
+</section>
 </body>
 </html>
